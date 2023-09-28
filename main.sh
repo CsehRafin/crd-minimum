620a0deb0e24ef714694b3f2e25dc1b22d46035c
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2023-09-28 15:59:55 +0600

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install neofetch wget -y
+sudo apt install xfce4 -y
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb
+clear
+echo everything good just run command from chrome remote desktop
